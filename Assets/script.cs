f2e2c39a40565c7d439e1bee0749ca7f25f6c55d
202004g04 <66997717+202004g04@users.noreply.github.com>
2020-10-06 20:11:35 +0900

    Initial commit

---
+++ Assets/script.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour
+{
+    [SerializeField]
+    Sprite[] cards;
+
+    public int number;
+    public int mark;
+    Sprite card;
+
+    //tart is called before the first frame update
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = cards[0];
+
+        card = cards[1];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
