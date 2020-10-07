d7f424695cb2d01c99035a79d8ab84c00ab436f9
202004g04 <66997717+202004g04@users.noreply.github.com>
2020-10-07 18:53:02 +0900

    Controllerの作成

---
+++ Assets/Scripts/CharacterBase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterBase : MonoBehaviour
+{
+    [SerializeField]
+    Text nameText;
+    protected string characterName;
+
+    [SerializeField]
+    Text scoreText;
+    protected int characterScore;
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        nameText.text = characterName;
+        scoreText.text = "---";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/DealerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DealerController : CharacterBase
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        characterName = "DEALER";
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
